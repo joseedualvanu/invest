fe668a930c97e1d306cb5fac8b5f85a01aa00668
jose.alvarez <joseedualvanu@gmail.com>
2022-04-01 13:40:25 -0300

    Primer carga

---
+++ plot.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+# Load the data
+bitcoin_data = pd.read_csv('bitcoin.csv', index_col='Date', parse_dates=True)
+
+# Print the top 5 rows
+print(bitcoin_data.head())
+
+"""
+# Plot the daily high price
+plt.plot(bitcoin_data['High'], color='green')
+# Plot the daily low price
+plt.plot(bitcoin_data['Low'], color='red')
+
+plt.title('Daily high low prices')
+plt.show()
+"""
+
+# Define the candlestick data
+candlestick = go.Candlestick(
+    x=bitcoin_data.index,
+    open=bitcoin_data['Open'],
+    high=bitcoin_data['High'],
+    low=bitcoin_data['Low'],
+    close=bitcoin_data['Close'])
+
+# Create a candlestick figure   
+fig = go.Figure(data=[candlestick])
+fig.update_layout(title='Bitcoin prices')                        
+
+# Show the plot
+fig.show()
