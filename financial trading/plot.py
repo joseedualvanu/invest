b7dd2e30fad58cd7ead37686a1a77dc3cb98a4fd
jose.alvarez <joseedualvanu@gmail.com>
2022-04-01 15:38:24 -0300

    Carga primeros ejercicios

--- financial trading/plot.py
+++ financial trading/plot.py
@@ -8,7 +8,7 @@ bitcoin_data = pd.read_csv('bitcoin.csv', index_col='Date', parse_dates=True)
 # Print the top 5 rows
 print(bitcoin_data.head())
 
-"""
+# """
 # Plot the daily high price
 plt.plot(bitcoin_data['High'], color='green')
 # Plot the daily low price
@@ -16,15 +16,18 @@ plt.plot(bitcoin_data['Low'], color='red')
 
 plt.title('Daily high low prices')
 plt.show()
-"""
+plt.savefig("fig1.png")
+# """
 
+# """
 # Define the candlestick data
 candlestick = go.Candlestick(
     x=bitcoin_data.index,
     open=bitcoin_data['Open'],
     high=bitcoin_data['High'],
     low=bitcoin_data['Low'],
-    close=bitcoin_data['Close'])
+    close=bitcoin_data['Price']
+    )
 
 # Create a candlestick figure   
 fig = go.Figure(data=[candlestick])
@@ -32,3 +35,5 @@ fig.update_layout(title='Bitcoin prices')
 
 # Show the plot
 fig.show()
+plt.savefig("fig2.png")
+# """
