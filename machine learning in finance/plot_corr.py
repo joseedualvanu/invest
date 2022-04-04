3c70a3b1f265c3ccc193cd838a685b7c710aa9b8
jose.alvarez <joseedualvanu@gmail.com>
2022-04-04 11:54:43 -0300

    Plot

--- machine learning in finance/plot_corr.py
+++ machine learning in finance/plot_corr.py
@@ -1,13 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot  as plt
+
+bitcoin_data = pd.read_csv('bitcoin_V1.csv', index_col='Date', parse_dates=True)
+
+# print(bitcoin_data.head())  # examine the SPY DataFrame
+
+# Plot the Adj_Close columns for SPY and LNG
+fig1 = bitcoin_data["Adj_Close"].plot(label='Bitcoin', legend=True)
+plt.show()  # show the plot
+plt.clf()  # clear the plot space
+plt.savefig("fig1.png")
+
+
+# Histogram of the daily price change percent of Adj_Close for LNG
+bitcoin_data['Adj_Close'].pct_change(10).plot.hist(bins=50)
+plt.xlabel('adjusted close 1-day percent change')
+plt.show()
+plt.savefig("fig2.png")
 
 # Create 5-day % changes of Adj_Close for the current day, and 5 days in the future
-lng_df['5d_future_close'] = lng_df['Adj_Close'].shift(-5)
-lng_df['5d_close_future_pct'] = lng_df['5d_future_close'].pct_change(5)
-lng_df['5d_close_pct'] = lng_df['Adj_Close'].pct_change(5)
+bitcoin_data['5d_future_close'] = bitcoin_data['Adj_Close'].shift(-5)
+bitcoin_data['5d_close_future_pct'] = bitcoin_data['5d_future_close'].pct_change(5)
+bitcoin_data['5d_close_pct'] = bitcoin_data['Adj_Close'].pct_change(5)
 
 # Calculate the correlation matrix between the 5d close pecentage changes (current and future)
-corr = lng_df[['5d_close_pct', '5d_close_future_pct']].corr()
+corr = bitcoin_data[['5d_close_pct', '5d_close_future_pct']].corr()
 print(corr)
 
 # Scatter the current 5-day percent change vs the future 5-day percent change
-plt.scatter(lng_df['5d_close_pct'], lng_df["5d_close_future_pct"])
+plt.scatter(bitcoin_data['5d_close_pct'], bitcoin_data["5d_close_future_pct"])
 plt.show()
+plt.savefig("fig3.png")
