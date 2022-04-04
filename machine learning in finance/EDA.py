abfb13f61055c40ba8f45ae863314efcde1d3773
jose.alvarez <joseedualvanu@gmail.com>
2022-04-04 18:56:54 -0300

    EDA & features and target

--- machine learning in finance/EDA.py
+++ machine learning in finance/EDA.py
@@ -5,18 +5,18 @@ bitcoin_data = pd.read_csv('bitcoin_V1.csv', index_col='Date', parse_dates=True)
 
 # print(bitcoin_data.head())  # examine the SPY DataFrame
 
-# Plot the Adj_Close columns for SPY and LNG
-fig1 = bitcoin_data["Adj_Close"].plot(label='Bitcoin', legend=True)
+# Plot the Adj_Close columns for bitcoins
+bitcoin_data["Adj_Close"].plot(label='Bitcoin', legend=True)
 plt.show()  # show the plot
 plt.clf()  # clear the plot space
-plt.savefig("fig1.png")
+# plt.savefig("fig1.png")
 
 
-# Histogram of the daily price change percent of Adj_Close for LNG
+# Histogram of the daily price change percent of Adj_Close for bitcoins
 bitcoin_data['Adj_Close'].pct_change(10).plot.hist(bins=50)
 plt.xlabel('adjusted close 1-day percent change')
 plt.show()
-plt.savefig("fig2.png")
+# plt.savefig("fig2.png")
 
 # Create 5-day % changes of Adj_Close for the current day, and 5 days in the future
 bitcoin_data['5d_future_close'] = bitcoin_data['Adj_Close'].shift(-5)
@@ -30,4 +30,4 @@ print(corr)
 # Scatter the current 5-day percent change vs the future 5-day percent change
 plt.scatter(bitcoin_data['5d_close_pct'], bitcoin_data["5d_close_future_pct"])
 plt.show()
-plt.savefig("fig3.png")
+# plt.savefig("fig3.png")
---
+++ machine learning in finance/features_target.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import talib
+
+bitcoin_data = pd.read_csv('bitcoin_V1.csv', index_col='Date', parse_dates=True)
+
+feature_names = ['5d_close_pct']  # a list of the feature names for later
+
+# Create moving averages and rsi for timeperiods of 14, 30, 50, and 200
+for n in [14, 30, 50, 200]:
+
+    # Create the moving average indicator and divide by Adj_Close
+    bitcoin_data['ma' + str(n)] = talib.SMA(bitcoin_data['Adj_Close'].values,
+                              timeperiod=n) / bitcoin_data['Adj_Close']
+    # Create the RSI indicator
+    bitcoin_data['rsi' + str(n)] = talib.RSI(bitcoin_data['Adj_Close'].values, timeperiod=n)
+    
+    # Add rsi and moving average to the feature name list
+    feature_names = feature_names + ['ma' + str(n), 'rsi' + str(n)]
+    
+print(feature_names)
